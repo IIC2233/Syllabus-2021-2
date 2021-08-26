26e76f2afa6c88c7d133a4325852b4618afd419e
Ian Basly <igbasly@uc.cl>
2021-08-26 15:30:12 -0400

    AP0 Publicada :star:

---
+++ Actividades/AP0/main.py
@@ -0,0 +1,29 @@
+# Debes completar esta función para que retorne la información de los ayudantes
+def cargar_datos(path):
+    pass
+
+
+# Completa esta función para encontrar la información del ayudante entregado
+def buscar_info_ayudante(nombre_ayudante, lista_ayudantes):
+    pass
+
+
+# Completa esta función para que los ayudnates puedan saludar
+def saludar_ayudante(info_ayudante):
+    pass
+
+
+if __name__ == '__main__':
+    pass
+    # El código que aquí escribas se ejecutará solo al llamar a este módulo.
+    # Aquí puedes probar tu código llamando a las funciones definidas.
+
+    # Puede llamar a cargar_datos con el path del archivo 'ayudantes.csv'
+    # para probar si obtiene bien los datos.
+
+    # Puedes intentar buscar la lista de unos de los nombres
+    # que se encuentran en el archivo con la función buscar_info_ayudante.
+    # Además puedes utilizar la lista obtenida para generar su saludo.
+
+    # Hint: la función print puede se útil para revisar
+    #       lo que se está retornando.
